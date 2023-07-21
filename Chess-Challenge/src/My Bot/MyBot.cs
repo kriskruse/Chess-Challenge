35e76c8dd3086374a5640a12a0340b98d0daaa53
Kris Kruse <kriskruse@hotmail.com>
2023-07-21 20:28:33 +0200

    Init my bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,23 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using System.Collections.Generic;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
+    // Piece values: null, pawn, knight, bishop, rook, queen, king
+    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] legalMoves = board.GetLegalMoves();
+        Dictionary<Move, int> evaluation = new Dictionary<Move, int>();
+        
+        foreach (Move legalMove in legalMoves)
+        {
+            evaluation.Add(legalMove, Math.Max());
+        }
+
+
+        return 
     }
 }
