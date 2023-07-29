694764288019d8d7c44bd14ed5f94dcb1e0dea20
Kris Kruse <kriskruse@hotmail.com>
2023-07-29 21:27:12 +0200

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,43 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using System.Threading;
 using ChessChallenge.API;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Raylib_cs;
 using Timer = ChessChallenge.API.Timer;
 
+/// <summary>
+/// Trying to cached visited boards, and the move that was deemed optimal by the model
+/// </summary>
+public struct cachedBoard
+{
+    public cachedBoard(Board b, Move m, int s)
+    {
+        board = b;
+        move = m;
+        score = s;
+    }
+    public Board board { get; }
+    public Move move { get; }
+    public int score { get; }
+}
+
+
+
+// TODO: We need a more elaborate evaluation function, one that takes into account the position on the board
+// TODO: We need to take the next positions into account
+// TODO: The evaluation of the moves need to be threaded to keep up to speed.
 public class MyBot : IChessBot{
     // Piece values: null, pawn, knight, bishop, rook, queen, king
-    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    int[] pieceValues = { 0, 100, 320, 330, 500, 900, 20000 };
     
     // We could hardCode some openings in here???
     // prob need a check to see if board is a standard start
-    private ulong bitBoard;
+    private ulong myBoard;
+    private ulong oppBoard;
+    private ulong allBoard;
     private bool doingOpener;
     private int openingIndex = 0;
-    private List<Move> superGoodStrongOpening = new();
+    private static readonly double _gamma = 0.9;
     
+    // These two are the hardcoded Italian game openers
+    //https://www.chess.com/openings/Italian-Game
+    private List<String> _OpeningWhite = new List<string>{"e2e4", "g1f3","f1c4"};
+    private List<String> _OpeningBlack = new List<string> { "e7e5", "b8c6", "f8c5"};
+    
+    // This is for caching moves
+    private Dictionary<Board, cachedBoard> _cache = new();
+
+    private Random rng = new();
+
+
     public Move Think(Board board, Timer timer)
     {
+        bool isWhite = board.IsWhiteToMove;
         // get the bitBoards
-        if (board.IsWhiteToMove)
-            bitBoard = board.WhitePiecesBitboard;
-        else bitBoard = board.BlackPiecesBitboard;
-        
+        if (isWhite)
+        {
+            myBoard = board.WhitePiecesBitboard;
+            oppBoard = board.BlackPiecesBitboard;
+        }
+        else
+        {
+            myBoard = board.BlackPiecesBitboard;
+            oppBoard = board.WhitePiecesBitboard;
+        }
+
         // if white or black board is a standard setup
-        if (bitBoard == 18446462598732840960 || bitBoard == 18446462598732906495 || doingOpener)
+        if (board.AllPiecesBitboard == 18446462598732906495 || myBoard == 18446462598732840960 || doingOpener)
         {
             doingOpener = true;
-            openingIndex++;
-            // make sure we only run through the opening and dont try to acces out of bounds
-            if (openingIndex == superGoodStrongOpening.Count)
+            if (openingIndex < _OpeningWhite.Count)
             {
-                doingOpener = false;
+                Move Open = isWhite
+                    ? new Move(_OpeningWhite[openingIndex], board)
+                    : new Move(_OpeningBlack[openingIndex], board);
+                openingIndex++;
+                return Open;
             }
-            // do some opening move based on index
-            
+            // when we are done with the opener, set the bool to false
+            doingOpener = false;
         }
         
-        return SingleDepthSearch(board, timer)[0];
+        // return the best move possible given the board
+        return EvaluateBoard(board,10).Item1;
     }
     bool MoveIsCheckmate(Board board, Move move)
     {
@@ -47,43 +97,49 @@ public class MyBot : IChessBot{
         return isMate;
     }
 
-    List<Move> SingleDepthSearch(Board board, Timer timer)
+    (Move, double) EvaluateBoard(Board board, int n)
     {
-        Random rng = new();
-        Dictionary<Move, int> evaluation = new();
-
-        Move[] legalMoves = board.GetLegalMoves();
-        List<int> moveVals = new();
-        List<Move> moveList = new();
+        // check if we have seen the board before, if so return the move we assumed best
+        if (_cache.ContainsKey(board)) return (_cache[board].move, _cache[board].score);
         
-
-        foreach (Move legalMove in legalMoves)
+        Move bestMoveOnBoard = new Move();
+        double bestMoveScore = 0;
+        double currentMoveScore = 0;
+        Move[] legalMoves = board.GetLegalMoves();
+        
+        foreach (var legalmove in legalMoves)
         {
-            // If the move is checkmate do the move
-            if (MoveIsCheckmate(board, legalMove))
+            // For every move evaluate the current worth
+            currentMoveScore = MoveEvaluation(board, legalmove);
+            if (currentMoveScore > bestMoveScore)
             {
-                moveList.Add(legalMove);
-                return moveList;
+                bestMoveOnBoard = legalmove;
+                bestMoveScore = currentMoveScore;
             }
-            
-            // else evaluate the move based on the captured piece
-            Piece capturedPiece = board.GetPiece(legalMove.TargetSquare);
-            int capturedPieceVal = pieceValues[(int)capturedPiece.PieceType];
-            evaluation.Add(legalMove, capturedPieceVal);
-            moveVals.Add(capturedPieceVal);
+            // As well as see the board if the move was chosen
+            // Then do the same evaluation and add it to the older moves score weighted by some Gamma
         }
-        // Get the move with the highest value
-        Move bestMove = evaluation.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-        if (evaluation[bestMove] == 0) moveList.Add(legalMoves[rng.Next(legalMoves.Length)]);
-        else
-        {
-            double avg = moveVals.Count > 0 ? moveVals.Average() : 0.0;
-            foreach (Move key in evaluation.Keys)
-            {
-                if (evaluation[key] >= avg) moveList.Add(key);
-            }
-        }
-        return moveList;
 
+        if (bestMoveScore == 0) bestMoveOnBoard = legalMoves[rng.Next(legalMoves.Length)];
+        // to catch null moves 
+        if (bestMoveOnBoard == Move.NullMove) throw new ArgumentException("We somehow made a null move");
+        
+        return (bestMoveOnBoard, bestMoveScore);
     }
+
+    double MoveEvaluation(Board board, Move move)
+    {
+        double score = 0;
+        // return a score for a move on the given board
+        if (MoveIsCheckmate(board, move)) return double.PositiveInfinity; // Checkmate is perfect score
+        
+        // step one, check if we capture a piece
+        // Find highest value capture
+        Piece capturedPiece = board.GetPiece(move.TargetSquare);
+        score += pieceValues[(int)capturedPiece.PieceType];
+
+        return score;
+    }
+    
+    
 }
